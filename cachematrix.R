2711b40860e1259797c56ec1206700cd759926ac
mawasae <69472197+mawasae@users.noreply.github.com>
2020-08-17 23:46:02 +0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My functions are similar to that described in coursera example... 
+## in the function below , m is set to null when matrix has never been computed but if it is computed before thenn the setinverse function changes the value of m in the parent environment to computed value.
+makecachematrix=function(x=matrix()){
+       m=NULL
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       getx=x
+       setx=function(y){
+              x<<-y
+              m<<-NULL
+       }
+       setinverse=function(inverse){
+              m<<-inverse
+       }
+       getinverse=m
+       list(getx=getx,setx=setx,setinverse=setinverse,getinverse=getinverse)
+        
+        ## This function will provide 4 functions as mentioned in above list.. m is set to null whenever the matrix is not calculated for the first time and will always have a value when computed before.
+}       
+cachesolve=function(x=matrix()){
+      A= makecachematrix(x)
+      inverse=A$getinverse
+      if(!is.null(inverse)){
+             
+             return(inverse)
+      }
+      B=makecachematrix(x)
+      matrix=B$getx
+      mat=solve(matrix)
+      makecachematrix(x)$setinverse(mat)
+      mat
 }
